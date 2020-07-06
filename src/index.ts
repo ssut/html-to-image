e7799dcb039aea18e1ce774bf568d98e190ac94b
SuHun Han <ssut@ssut.me>
2020-07-07 02:03:08 +0900

    refactor: to async functions

--- src/index.ts
+++ src/index.ts
@@ -50,92 +50,97 @@ function getImageSize(domNode: HTMLElement, options: OptionsType = {}) {
   return { width, height };
 }
 
-export function toSvgDataURL(
+export async function toSvgDataURL(
   domNode: HTMLElement,
-  options: OptionsType = {}
+  options: OptionsType = {},
 ): Promise<string> {
   const { width, height } = getImageSize(domNode, options);
 
-  return cloneNode(domNode, options.filter, true)
-    .then((clonedNode) => embedWebFonts(clonedNode!, options))
-    .then((x: any) => {
-      console.info('embedded web fonts');
-      return x;
-    })
-    .then((clonedNode) => embedImages(clonedNode, options))
-    .then((x: any) => {
-      console.info('embedded images');
-      return x;
-    })
-    .then((clonedNode) => applyStyleWithOptions(clonedNode, options))
-    .then((x: any) => {
-      console.info('embedded styles');
-      return x;
-    })
-    .then((clonedNode) => createSvgDataURL(clonedNode, width, height));
+  let clonedNode = (await cloneNode(domNode, options.filter, true))!;
+
+  // embed web fonts
+  clonedNode = await embedWebFonts(clonedNode, options);
+
+  // embed images
+  clonedNode = await embedImages(clonedNode, options);
+
+  // apply styles
+  clonedNode = await applyStyleWithOptions(clonedNode, options);
+
+  // create svgDataURL
+  const svgDataURL = await createSvgDataURL(clonedNode, width, height);
+
+  return svgDataURL;
 }
 
-export function toCanvas(
+export async function toCanvas(
   domNode: HTMLElement,
-  options: OptionsType = {}
+  options: OptionsType = {},
 ): Promise<HTMLCanvasElement> {
-  return toSvgDataURL(domNode, options)
-    .then(createImage)
-    .then(delay(100))
-    .then((image) => {
-      const canvas = document.createElement('canvas');
-      const context = canvas.getContext('2d')!;
-      const ratio = getPixelRatio();
-      const { width, height } = getImageSize(domNode, options);
+  const svgDataURL = await toSvgDataURL(domNode, options);
+  const image = await createImage(svgDataURL);
 
-      canvas.width = width * ratio;
-      canvas.height = height * ratio;
-      canvas.style.width = `${width}`;
-      canvas.style.height = `${height}`;
-      context.scale(ratio, ratio);
+  await delay(100);
 
-      if (options.backgroundColor) {
-        context.fillStyle = options.backgroundColor;
-        context.fillRect(0, 0, canvas.width, canvas.height);
-      }
+  const canvas = document.createElement('canvas');
+  const context = canvas.getContext('2d')!;
+  const ratio = getPixelRatio();
+  const { width, height } = getImageSize(domNode, options);
 
-      context.drawImage(image, 0, 0);
+  canvas.width = width * ratio;
+  canvas.height = height * ratio;
+  canvas.style.width = `${width}`;
+  canvas.style.height = `${height}`;
+  context.scale(ratio, ratio);
 
-      return canvas;
-    });
+  if (options.backgroundColor) {
+    context.fillStyle = options.backgroundColor;
+    context.fillRect(0, 0, canvas.width, canvas.height);
+  }
+
+  context.drawImage(image, 0, 0);
+
+  return canvas;
 }
 
-export function toPixelData(
+export async function toPixelData(
   domNode: HTMLElement,
-  options: OptionsType = {}
+  options: OptionsType = {},
 ): Promise<Uint8ClampedArray> {
   const { width, height } = getImageSize(domNode, options);
-  return toCanvas(domNode, options).then(
-    (canvas) => canvas.getContext('2d')!.getImageData(0, 0, width, height).data
-  );
+  const canvas = await toCanvas(domNode, options);
+
+  const pixelData = canvas.getContext('2d')!.getImageData(0, 0, width, height).data;
+  return pixelData;
 }
 
-export function toPng(
+export async function toPng(
   domNode: HTMLElement,
-  options: OptionsType = {}
+  options: OptionsType = {},
 ): Promise<string> {
-  return toCanvas(domNode, options).then((canvas) => canvas.toDataURL());
+  const canvas = await toCanvas(domNode, options);
+
+  const pngDataURL = canvas.toDataURL();
+  return pngDataURL;
 }
 
-export function toJpeg(
+export async function toJpeg(
   domNode: HTMLElement,
-  options: OptionsType = {}
+  options: OptionsType = {},
 ): Promise<string> {
-  return toCanvas(domNode, options).then((canvas) =>
-    canvas.toDataURL('image/jpeg', options.quality || 1)
-  );
+  const canvas = await toCanvas(domNode, options);
+
+  const jpegDataUrl = canvas.toDataURL('image/jpeg', options.quality || 1);
+  return jpegDataUrl;
 }
 
-export function toBlob(
+export async function toBlob(
   domNode: HTMLElement,
-  options: OptionsType = {}
+  options: OptionsType = {},
 ): Promise<Blob | null> {
-  return toCanvas(domNode, options).then(canvasToBlob);
+  const canvas = await toCanvas(domNode, options);
+
+  return canvasToBlob(canvas);
 }
 
 export default {
